c21c32a2e31385587652a6c862a621099978b5a0
Theo de Souza <theo.souza@alunos.ifsuldeminas.edu.br>
2026-04-06 11:16:24 -0300

    Inicio do sistema de cadastro/login

---
+++ src/main/java/br/edu/tds/usuario/Main.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.tds.usuario;
+
+public class Main {
+
+   public static void main(String[] args) {
+
+       UsuarioDAO dao = new UsuarioDAO();
+
+       // CADASTRO
+       Usuario u1 = new Usuario("Carlos", "carlos@email.com", "1234");
+       dao.cadastrar(u1);
+
+       // LOGIN
+       boolean acesso = dao.login("carlos@email.com", "1234");
+
+       if (acesso) {
+           System.out.println("Login realizado com sucesso!");
+       } else {
+           System.out.println("Email e/ou senha inválido(s)!");
+       }
+   }
+}
+
